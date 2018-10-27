3d6cf0fb98d7800aa3ed93f1fa1ceed02e9a43bb
bgmarti <bgmarti@onet.eu>
2018-10-27 22:04:30 +0200

    added simple code examples to be refactored using: - builder - static factory method - factory method - abstract factory

---
+++ src/main/java/edu/agh/wfiis/solid/designpatterns/creational/factoryMethod/DesignPattern.java
@@ -0,0 +1,99 @@
+package edu.agh.wfiis.solid.designpatterns.creational.factoryMethod;
+
+import java.util.Objects;
+
+public class DesignPattern {
+
+    private final String name;
+
+    private final RunnableCode patternCode;
+
+    public RunnableCode refactor(String badCode) {
+        if (patternCode instanceof JavaCode) {
+            return JavaEnvironment.run(patternCode.run(badCode));
+        } else if (patternCode instanceof PythonCode) {
+            return PythonEnvironment.run(patternCode.run(badCode));
+        }
+        return null;
+    }
+
+    public DesignPattern(String name, String patternCode, String language) {
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(patternCode);
+        Objects.requireNonNull(language);
+
+        this.name = name;
+
+        if (language.equalsIgnoreCase("java")) {
+            JavaEnvironment.setUp();
+            this.patternCode = JavaEnvironment.compile(patternCode);
+        } else if (language.equalsIgnoreCase("python")) {
+            PythonEnvironment.setUp();
+            this.patternCode = PythonEnvironment.compile(patternCode);
+        } else if (language.equalsIgnoreCase("c#")) {
+            throw new UnsupportedOperationException("No fkng way!");
+        } else {
+            throw new IllegalArgumentException("Unknown language");
+        }
+    }
+
+
+    public static void main(String args[]) {
+        DesignPattern designPattern = new DesignPattern("builder", " public Builder {....", "java");
+        designPattern.refactor("public class ClassWithManyConstructors...");
+    }
+}
+
+
+//required dependencies, put here only to compile project
+interface RunnableCode {
+    RunnableCode run(String code);
+}
+
+class PythonCode implements RunnableCode {
+    @Override
+    public RunnableCode run(String code) {
+        //magic happens here
+        return null;
+    }
+}
+
+class JavaCode implements RunnableCode {
+    @Override
+    public RunnableCode run(String code) {
+        //magic happens here
+        return null;
+    }
+}
+
+class PythonEnvironment {
+    static void setUp() {
+        //magic happens here
+    }
+
+    static RunnableCode compile(String code) {
+        //magic happens here
+        return new PythonCode();
+    }
+
+    static RunnableCode run(RunnableCode code) {
+        //magic happens here
+        return new PythonCode();
+    }
+}
+
+class JavaEnvironment {
+    static void setUp() {
+        //magic happens here
+    }
+
+    static RunnableCode compile(String code) {
+        //magic happens here
+        return new JavaCode();
+    }
+
+    static RunnableCode run(RunnableCode code) {
+        //magic happens here
+        return new JavaCode();
+    }
+}
